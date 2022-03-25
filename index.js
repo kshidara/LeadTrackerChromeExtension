33528f39773f1619f58c53e1f32e2f6b0996f59c
Kazune Shidara <kazuneshidara@gmail.com>
2022-03-25 01:09:26 -0700

    added button functionality by creating DOM event listener onclick function

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-index.js
+
+
+
+let myLeads = []
+const inputEl = document.getElementById("input-el")  //const can't be reassigned
+const inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+
+    myLeads.push(inputEl.value)
+
+})
+
+
+
+
+
+
