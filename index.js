8cec64d4d17e32b18d9b7dcdddf014217dab1bc4
Kazune Shidara <kazuneshidara@gmail.com>
2022-03-26 12:16:39 -0700

    implemented innerHTML to list out array items in list format

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 
 
 
-let myLeads = []
-const inputEl = document.getElementById("input-el")  //const can't be reassigned
-const inputBtn = document.getElementById("input-btn")
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+const inputEl = document.getElementById("input-el");  //const can't be reassigned
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function() {
 
@@ -12,8 +13,9 @@ inputBtn.addEventListener("click", function() {
 })
 
 for (let i = 0; i < myLeads.length; i++) {
+
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
     
-    console.log(myLeads[i]);
 }
 
 
