b1b0f4edb5b6d2a022972ea61e402657f75f7244
Kazune Shidara <kazuneshidara@gmail.com>
2022-03-31 21:20:42 -0700

    implemented local storage functionality as practice

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-
-
+// chrome://extensions/
 
 let myLeads = []
 const inputEl = document.getElementById("input-el");  //const can't be reassigned
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+
+// global variable,set key and variable as strings (primitive db so it only takes stringss as key/value)
+localStorage.setItem("myLeads", "www.examplelead.com")
+console.log(localStorage.getItem("myLeads"))
+localStorage.clear()
 
 inputBtn.addEventListener("click", function() {
 
